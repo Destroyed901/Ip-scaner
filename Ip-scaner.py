813d3d3140f41304c32c0152015967fee747a522
Destroyer901 <76010338+Destroyed901@users.noreply.github.com>
2021-01-29 21:02:51 +0200

    Add files via upload

---
+++ Ip-scaner.py
@@ -0,0 +1,133 @@
+import requests
+import json
+import os
+from termcolor import colored
+from colorama import Fore, Back, Style
+def ip():
+  print('Введите айпи.')
+  ip = input('>>> ')
+  response = requests.get('https://ipinfo.io/' + ip + '/json')
+  r = response.json()
+  try:
+    try:
+      print('[IP] : ' + r['ip'])
+    except:
+      pass
+    try:
+      print('[Страна] : ' + r['country'])
+    except:
+      pass
+    try:
+      print('[Регион] : ' + r['region'])
+    except:
+      pass
+    try:
+      print('[Город] : ' + r['city'])
+    except:
+      pass
+    try:
+      print('[Имя устройства] : ' + r['hostname'])
+    except:
+      pass
+    try:
+      print('[Местоположение] : ' + r['loc'])
+    except:
+      pass
+    try:
+      print('[Провайдер] : ' + r['org'])
+    except:
+      pass
+    try:
+      print('[Часовой пояс] : ' + r['timezone'])
+    except:
+      pass
+    try:
+      print('[Почтовый индекс] : ' +  r['postal'])
+    except:
+      pass
+  except Exeption as er:
+    print(er)
+  print('----------')
+  main()
+
+def self_ip():
+  response = requests.get('https://ipinfo.io/json')
+  r = response.json()
+  try:
+    try:
+      print('[IP] : ' + r['ip'])
+    except:
+      pass
+    try:
+      print('[Страна] : ' + r['country'])
+    except:
+      pass
+    try:
+      print('[Регион] : ' + r['region'])
+    except:
+      pass
+    try:
+      print('[Город] : ' + r['city'])
+    except:
+      pass
+    try:
+      print('[Имя устройства] : ' + r['hostname'])
+    except:
+      pass
+    try:
+      print('[Местоположение] : ' + r['loc'])
+    except:
+      pass
+    try:
+      print('[Провайдер] : ' + r['org'])
+    except:
+      pass
+    try:
+      print('[Часовой пояс] : ' + r['timezone'])
+    except:
+      pass
+    try:
+      print('[Почтовый индекс] : ' +  r['postal'])
+    except:
+      pass
+  except Exeption as er:
+    print(er)
+  print('----------')
+  main()
+
+def main():
+  print('[1] IP scan')
+  print('[2] Yours IP')
+  print('[0] Exit')
+  a = input('>>> ')
+  if a == '1':
+    ip()
+  elif a == '2':
+    self_ip()
+  elif a == '0':
+    print('Завершение программы...')
+    
+  else:
+    print('Неверная команда.')
+    main()
+
+def clear():
+  if os.sys.platform == "win32":
+    os.system("cls")
+  else:
+    os.system("clear")
+ 
+banner = colored("""
+
+
+  ___                                           
+ |_ _|_ __        ___  ___ __ _ _ __   ___ _ __ 
+  | || '_ \ _____/ __|/ __/ _` | '_ \ / _ \ '__|
+  | || |_) |_____\__ \ (_| (_| | | | |  __/ |   
+ |___| .__/      |___/\___\__,_|_| |_|\___|_|   
+     |_|                                        
+
+       telegram channel: @termux999
+""", "green")
+print(banner)
+main()
